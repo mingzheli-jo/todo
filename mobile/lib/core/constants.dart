20b4b303a6c798109decd83b23ceda70a1331ee4
jo.li <jo.li@kuaikuenglish.com>
2026-05-29 21:56:11 +0800

    chore(mobile): point app to cloud API endpoint
    Switch apiBaseUrl from LAN dev address to https://todo.azhefuye.online/api
    so the installed app talks to the live cloud backend instead of a local
    host. LAN/emulator addresses retained as commented dev fallbacks.

--- mobile/lib/core/constants.dart
+++ mobile/lib/core/constants.dart
@@ -1,6 +1,7 @@
 class AppConstants {
-  static const String apiBaseUrl = 'http://192.168.31.60:8000/api';
-  // Cloud (when deployed): 'https://todo.azhefuye.online/api'
-  // Android emulator → host: 'http://10.0.2.2:8000/api'
+  static const String apiBaseUrl = 'https://todo.azhefuye.online/api';
+  // Dev fallbacks:
+  //   LAN host:           'http://192.168.31.60:8000/api'
+  //   Android emulator:   'http://10.0.2.2:8000/api'
   static const String tokenStorageKey = 'toto_token';
 }
